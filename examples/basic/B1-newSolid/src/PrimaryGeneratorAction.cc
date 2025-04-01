dc0266c6b0e0228973dffa4bb4b8fc642e32d28f
georgeweis <george.weis@student.manchester.ac.uk>
2025-04-01 14:21:46 +0100

    increased point size on visualiser to highlight interaction points (to chekc theyre all on the surface as expected), in vis.mac and changed the particle type to geantino in PrimaryGeneratorAction.cc

--- examples/basic/B1-newSolid/src/PrimaryGeneratorAction.cc
+++ examples/basic/B1-newSolid/src/PrimaryGeneratorAction.cc
@@ -50,7 +50,7 @@ PrimaryGeneratorAction::PrimaryGeneratorAction()
   // default particle kinematic
   G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
   G4String particleName;
-  G4ParticleDefinition* particle = particleTable->FindParticle(particleName = "gamma");
+  G4ParticleDefinition* particle = particleTable->FindParticle(particleName = "geantino");
   fParticleGun->SetParticleDefinition(particle);
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0., 0., 1.));
   fParticleGun->SetParticleEnergy(6. * MeV);
